4abc19506861dbd7a6066891e2763a9b99df1f66
unknown <ryanjay333@gmail.com>
2020-08-26 13:07:43 -0400

    removed local imgs, added favicon.ico, changed grid-container width

--- src/components/title-container/title-container.component.jsx
+++ src/components/title-container/title-container.component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./title-container.styles.css";
-import logo from "../../logo.png";
+import logo from "../../img/logo.png";
 
 const titleContainer = () => {
   return (
